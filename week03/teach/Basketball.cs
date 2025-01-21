ec6ec2ae7c06892ce5b2080f1667c88dbe5bad5b
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-01-21 09:50:54 -0300

    modified:   week03/teach/Basketball.cs

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -27,10 +27,20 @@ public class Basketball
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+            if (players.ContainsKey(playerId))
+            {
+                players[playerId] += points;
+            }
+            else
+            {
+                players.Add(playerId, points);
+            }
         }
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+        //Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
 
-        var topPlayers = new string[10];
+        var dictOrdained = players.OrderByDescending(pair => pair.Value).Take(10).ToArray();
+
+       
     }
 }
